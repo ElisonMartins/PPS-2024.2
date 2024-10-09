d721f200ce35239a9fd28499cdc3431c5d8ebedc
elise <Elisenovaes10@gmail.com>
2024-10-09 18:52:19 -0300

    spring init

---
+++ cookingbook/src/main/java/br/com/cookingbook/demo/CookingbookApplication.java
@@ -0,0 +1,13 @@
+package br.com.cookingbook.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingbookApplication.class, args);
+	}
+
+}
