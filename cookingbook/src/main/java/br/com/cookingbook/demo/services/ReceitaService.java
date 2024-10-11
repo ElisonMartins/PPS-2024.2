82e00f6f149363bb3c5391eb55928ace859f4788
elise <Elisenovaes10@gmail.com>
2024-10-11 08:25:13 -0300

    Update ReceitaService.java

--- cookingbook/src/main/java/br/com/cookingbook/demo/services/ReceitaService.java
+++ cookingbook/src/main/java/br/com/cookingbook/demo/services/ReceitaService.java
@@ -1,5 +1,7 @@
 package br.com.cookingbook.demo.services;
 
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,4 +12,41 @@ public class ReceitaService {
      @Autowired
     private ReceitaRepository receitaRepository;
 
+    // Criar uma nova receita
+    public Receita criarReceita(Receita receita) {
+        return receitaRepository.save(receita);
+    }
+
+    // Obter todas as receitas
+    public List<Receita> listarReceitas() {
+        return receitaRepository.findAll();
+    }
+
+    // Obter receita por ID
+    public Optional<Receita> obterReceitaPorId(Long id) {
+        return receitaRepository.findById(id);
+    }
+
+    // Atualizar uma receita existente
+    public Receita atualizarReceita(Long id, Receita receitaAtualizada) {
+        Optional<Receita> receitaExistente = receitaRepository.findById(id);
+        
+        if (receitaExistente.isPresent()) {
+            Receita receita = receitaExistente.get();
+            receita.setTitulo(receitaAtualizada.getTitulo());
+            receita.setDescricao(receitaAtualizada.getDescricao());
+            receita.setIngredientes(receitaAtualizada.getIngredientes());
+            receita.setPreparo(receitaAtualizada.getPreparo());
+            receita.setTempo(receitaAtualizada.getTempo());
+            receita.setPorcoes(receitaAtualizada.getPorcoes());
+            return receitaRepository.save(receita);
+        } else {
+            throw new RuntimeException("Receita não encontrada");
+        }
+    }
+
+    // Deletar uma receita por ID
+    public void deletarReceita(Long id) {
+        receitaRepository.deleteById(id);
+    }
 }
