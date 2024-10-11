4257628591844a19d5265f429b2ba6f84ded3646
elise <Elisenovaes10@gmail.com>
2024-10-11 09:00:26 -0300

    Edições gerais

--- cookingbook/src/main/java/br/com/cookingbook/demo/services/ReceitaService.java
+++ cookingbook/src/main/java/br/com/cookingbook/demo/services/ReceitaService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.cookingbook.demo.entities.Receita;
 import br.com.cookingbook.demo.repositories.ReceitaRepository;
 
+@Service
 public class ReceitaService {
      @Autowired
     private ReceitaRepository receitaRepository;
